e10365878e760135007fb54aad3b4922eef9a51c
CaraiApp <info@caraimelilla.com>
2025-02-26 20:14:56 +0100

    Simplificación de next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  });
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-  };
-  
-  module.exports = withPWA(nextConfig);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Si necesitas configuraciones de imagen
+  images: {
+    domains: ['your-supabase-storage-domain.com'], // Ajusta según tus necesidades
+  }
+};
+
+module.exports = nextConfig;
